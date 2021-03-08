287e73ff66712c20f3c9940679e612ae6957d801
root <root@35k9.l.time4vps.cloud>
2021-03-08 13:23:04 +0200

    added chance for the bot to change its pfp when using +neko

--- src/commands/fun/neko.js
+++ src/commands/fun/neko.js
@@ -3,11 +3,23 @@ module.exports = {
     description: "returns a neko", 
     category: "fun", 
     execute: async(client, message, args) => {
+        const url = client.neko.nekos()
+        
+        if (Math.floor(Math.random() * 100) > 92) new Promise((res, rej) => {
+            client.user.setAvatar(url)
+            .then(() => res())
+            .catch(() => rej())
+            
+            setTimeout(() => {
+                rej()
+            }, 17500)
+        })
+        
         const embed = new client.discord.MessageEmbed()
         .setColor("RANDOM")
         .setTitle(`Here is a neko for ${message.author.username}:`)
-        .setImage(client.neko.nekos())
-        .setFooter(`Powered by nekos.best api`)
+        .setImage(url)
+        .setFooter(`Powered by nekos.best api ${Math.floor(Math.random() * 100) > 60 ? "| Using this too much may make me~" : ""}`)
         
         message.channel.send(embed)
     }
